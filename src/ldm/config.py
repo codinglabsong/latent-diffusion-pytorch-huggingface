4e2a2aa00075f0356de70da99b2c5897ffa801ae
John Song <sjjjsong@gmail.com>
2025-08-02 12:05:04 -0700

    added models

---
+++ src/ldm/config.py
@@ -0,0 +1,16 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+batch_size = 64
+img_size = 32
+
+latent_channels = 4
+
+lr_vae = 1e-4
+vae_epochs = 50
+
+lr_unet = 1e-4
+denoising_timesteps = 1000
+num_warmup_steps = 500
+unet_epochs = 100
